2ee028f2e4c0b2d5b7021b361bb6f3abda3ad313
grian <grianguy32@gmail.com>
2025-02-23 00:46:22 +0200

    fix: remove utility function for headers and put them in default move function

--- src/main/kotlin/Util.kt
+++
@@ -1,9 +0,0 @@
-package me.grian
-
-import io.ktor.http.*
-
-fun HeadersBuilder.applyBasicHeaders(token: String) = HeadersBuilder().apply {
-    set("Content-Type", "application/json")
-    set("Accept", "application/json")
-    set("Authorization", "Bearer $token")
-}
