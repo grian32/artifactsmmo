7895076a4d425fd41d587e6e21ff38073a615c74
grian <grianguy32@gmail.com>
2025-02-23 00:27:35 +0200

    feat: setup basic headers

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,12 @@ package me.grian
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
+import kotlin.io.path.Path
+import kotlin.io.path.readText
 
 fun main() {
     val client = HttpClient(CIO) {
@@ -12,4 +16,14 @@ fun main() {
             json(Json)
         }
     }
+
+    val env = Json.decodeFromString<Env>(Path("src/main/resources/env.json").readText())
+
+    val headers = HeadersBuilder().apply {
+        set("Content-Type", "application/json")
+        set("Accept", "application/json")
+        set("Authorization", "Bearer ${env.token}")
+    }
+
+    val characterName = "Grian"
 }
