d915f1684f5be20d614ce9d8fd61147db9ee5ab7
grian <grianguy32@gmail.com>
2025-03-31 04:04:13 +0300

    refactor: update imports

--- src/main/kotlin/data/move/MoveData.kt
+++ src/main/kotlin/data/move/MoveData.kt
@@ -1,6 +1,8 @@
 package me.grian.data.move
 
 import kotlinx.serialization.Serializable
+import me.grian.data.ArtifactsCharacter
+import me.grian.data.Cooldown
 
 @Serializable
 data class MoveData (
